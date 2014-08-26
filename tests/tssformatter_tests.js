19fb50a0437cd04b42a6963ab9ded64b00a27068
tido-dev <silverwolf90@gmail.com>
2014-08-26 16:33:57 +0100

    Extract out common tss test operations

--- tests/tssformatter_tests.js
+++ tests/tssformatter_tests.js
@@ -32,6 +32,26 @@ var voiceRanges = [
   [7, 10]
 ];
 
+function createNotes(notesData){
+  return notesData.map(function (spec) {
+    return new Vex.Flow.StaveNote({
+      keys: spec.keys,
+      duration: spec.duration,
+      auto_stem: true
+    });
+  });
+}
+
+function createVoices(voiceRanges, notes){
+  return voiceRanges.map(function (range) {
+    return new Vex.Flow.Voice({
+      num_beats: 3,
+      beat_value: 4,
+      resolution: Vex.Flow.RESOLUTION
+    }).addTickables(notes.slice(range[0], range[1]));
+  });
+}
+
 var TssFormatter = Vex.Flow.TssFormatter;
 
 Vex.Flow.Test.TssFormatter.Start = function() {
@@ -40,7 +60,7 @@ Vex.Flow.Test.TssFormatter.Start = function() {
   Vex.Flow.Test.runTest("Auto - desired", Vex.Flow.Test.TssFormatter.autoSpacing(TssFormatter.mode.AUTO_DESIRED));
   Vex.Flow.Test.runTest("Stretchy", Vex.Flow.Test.TssFormatter.stretchySpacing);
   test("Compare Widths", Vex.Flow.Test.TssFormatter.compareWidths);
-}
+};
 
 Vex.Flow.Test.TssFormatter.autoSpacing = function(mode) {
   return function (options, contextBuilder) {
@@ -54,21 +74,8 @@ Vex.Flow.Test.TssFormatter.autoSpacing = function(mode) {
 
     staves[0].addTrebleGlyph();
 
-    var notes = notesData.map(function (spec) {
-      return new Vex.Flow.StaveNote({
-        keys: spec.keys,
-        duration: spec.duration,
-        auto_stem: true
-      });
-    });
-
-    var voices = voiceRanges.map(function (range) {
-      return new Vex.Flow.Voice({
-        num_beats: 3,
-        beat_value: 4,
-        resolution: Vex.Flow.RESOLUTION
-      }).addTickables(notes.slice(range[0], range[1]));
-    });
+    var notes = createNotes(notesData);
+    var voices = createVoices(voiceRanges, notes);
 
     staves.forEach(function (stave, index) {
       formatter.format([voices[index]], index, stave);
@@ -90,22 +97,8 @@ Vex.Flow.Test.TssFormatter.stretchySpacing = function(options, contextBuilder){
 
   staves[0].addTrebleGlyph();
 
-  var notes = notesData.map(function(spec){
-    return new Vex.Flow.StaveNote({
-      keys: spec.keys, 
-      duration: spec.duration, 
-      auto_stem: true
-    });
-  });
-
-
-  var voices = voiceRanges.map(function(range){
-    return new Vex.Flow.Voice({
-      num_beats: 3,
-      beat_value: 4,
-      resolution: Vex.Flow.RESOLUTION
-    }).addTickables(notes.slice(range[0], range[1]));
-  });
+  var notes = createNotes(notesData);
+  var voices = createVoices(voiceRanges, notes);
 
   staves.forEach(function(stave, index){
     formatter.format([voices[index]], index, stave);
@@ -123,49 +116,23 @@ Vex.Flow.Test.TssFormatter.compareWidths = function(){
 
   staves[0].addTrebleGlyph();
 
-  var notes = notesData.map(function (spec) {
-    return new Vex.Flow.StaveNote({
-      keys: spec.keys,
-      duration: spec.duration,
-      auto_stem: true
-    });
-  });
-
-  var minimumVoices = voiceRanges.map(function (range) {
-    return new Vex.Flow.Voice({
-      num_beats: 3,
-      beat_value: 4,
-      resolution: Vex.Flow.RESOLUTION
-    }).addTickables(notes.slice(range[0], range[1]));
-  });
+  var notes = createNotes(notesData);
+  var minimumVoices = createVoices(voiceRanges, notes);
 
   staves.forEach(function (stave, index) {
     formatter.format([minimumVoices[index]], index, stave);
   });
 
   var availableWidth = staves.reduce(function(sum, stave){
-    return sum + stave.width
+    return sum + stave.width;
   }, 0);
 
   staves = formatter.getStaves(TssFormatter.mode.STRETCHY, [{clef: false}, {clef: false}, {clef: false}, {clef: false}], availableWidth);
 
   staves[0].addTrebleGlyph();
 
-  notes = notesData.map(function (spec) {
-    return new Vex.Flow.StaveNote({
-      keys: spec.keys,
-      duration: spec.duration,
-      auto_stem: true
-    });
-  });
-
-  var stretchyVoices = voiceRanges.map(function (range) {
-    return new Vex.Flow.Voice({
-      num_beats: 3,
-      beat_value: 4,
-      resolution: Vex.Flow.RESOLUTION
-    }).addTickables(notes.slice(range[0], range[1]));
-  });
+  notes = createNotes(notesData);
+  var stretchyVoices = createVoices(voiceRanges, notes);
 
   staves.forEach(function (stave, index) {
     formatter.format([stretchyVoices[index]], index, stave);
@@ -175,5 +142,5 @@ Vex.Flow.Test.TssFormatter.compareWidths = function(){
     minimumVoice.tickables.forEach(function(tickable, tickableIndex){
       equal(tickable.tickContext.x, stretchyVoices[voiceIndex].tickables[tickableIndex].tickContext.x);
     });
-  })
+  });
 };
